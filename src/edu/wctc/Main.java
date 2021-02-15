25ee4ba0d3035b171037aff055cc67ef7e6ee614
Nick Gorjestani <nickg2794@hotmail.com>
2021-02-14 19:21:40 -0600

    Updated control break function

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -1,10 +1,14 @@
 package edu.wctc;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
@@ -51,7 +55,7 @@ public class Main {
         String ans = keyboard.nextLine();
 
         try {
-            int ansNum = Integer.parseInt(ans);
+            int ansNum = parseInt(ans);
             if (ansNum < MealType.values().length) {
                 mealType = MealType.values()[ansNum - 1];
             }
@@ -92,7 +96,7 @@ public class Main {
                     searchByName();
                     break;
                 case "4":
-                    // doControlBreak();
+                    doControlBreak();
                     break;
                 case "5":
                     userContinue = false;
@@ -112,6 +116,78 @@ public class Main {
     }
 
     private void doControlBreak() {
+        List<Meal> mealList = new ArrayList<>();
 
+        try (Scanner fileInput = new Scanner(new File("meals_data.csv"))) {
+            while (fileInput.hasNext()) {
+                String line = fileInput.nextLine();
+                String[] columns = line.split(",");
+
+                String mealType = columns[0];
+                MealType typeOfMeal;
+                switch (mealType) {
+                    case "Breakfast":
+                        typeOfMeal = MealType.BREAKFAST;
+                        break;
+                    case "Dessert":
+                        typeOfMeal = MealType.DESSERT;
+                        break;
+                    case "Dinner":
+                        typeOfMeal = MealType.DINNER;
+                        break;
+                    case "Lunch":
+                        typeOfMeal = MealType.LUNCH;
+                        break;
+                    default:
+                        typeOfMeal = null;
+                }
+
+                String item = columns[1];
+                int calories = parseInt(columns[2]);
+
+                Meal meal = new Meal(typeOfMeal, item, calories);
+                mealList.add(meal);
+            }
+
+            String currentMealType = mealList.get(0).getMealType().getPrettyPrint();
+
+            System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%n", "Meal Type", "Total", "Mean", "Min", "Max", "Median");
+
+            int count = 0;
+            float total = 0;
+            float mean = 0;
+            float min = 0;
+            float max = 0;
+            float median = 0;
+
+            for (Meal meal : mealList) {
+                if (!currentMealType.equals(meal.getMealType().getPrettyPrint())) {
+                    mean = total / count;
+                    System.out.printf("%-20s%-20f%-20f%-20f%-20f%-20f%n", currentMealType, total, mean, min, max, median);
+
+                    total = 0;
+                    mean = 0;
+                    min = 0;
+                    max = 0;
+                    median = 0;
+                    count = 0;
+
+                    currentMealType = meal.getMealType().getPrettyPrint();
+                }
+
+                if (min == 0 || meal.getCalories() < min) {
+                    min = meal.getCalories();
+                }
+                if (meal.getCalories() > max) {
+                    max = meal.getCalories();
+                }
+                total += meal.getCalories();
+                count++;
+            }
+
+        } catch (IOException ioe) {
+            System.out.println("Can't open file");
+        }
     }
 }
+
